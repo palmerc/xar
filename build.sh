3b6d1b52a8f04290d154e8a4334d004071121012
Cameron Lowell Palmer <cameron@promon.no>
2025-04-28 16:23:33 +0200

    Kill the makefile in favor of CMake

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+pushd "$(dirname $0)" >/dev/null
+    SOURCE_DIR=$(pwd)
+popd >/dev/null
+
+BUILD_DIR=${SOURCE_DIR}/builddir
+INSTALL_DIR=${SOURCE_DIR}/dist
+
+rm -rf "${BUILD_DIR}" "${INSTALL_DIR}"
+mkdir -p "${BUILD_DIR}"
+pushd "${BUILD_DIR}" >/dev/null
+
+cmake \
+    -G Ninja \
+    -D CMAKE_VERBOSE_MAKEFILE=on \
+    -D CMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+    ${SOURCE_DIR}/xar
+cmake --build .
+cmake --build . --target install
+
+popd >/dev/null
+
